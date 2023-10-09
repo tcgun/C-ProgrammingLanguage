20d13d9cbbd08f2389da8ca119667c9c006f35aa
tcgun <129428964+tcgun@users.noreply.github.com>
2023-10-09 19:08:44 +0300

    Hello World application

---
+++ 1.0_merhaba-dunya.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // "Merhaba Dunya" metnini ekrana yazdırmak için printf fonksiyonu kullanılır.
+    printf("Merhaba Dunya");
+
+    // Program başarıyla tamamlandığını belirtmek için 0 döndürülür.
+    return 0;
+}
